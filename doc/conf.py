724cd3038b9aaffed9563bf7a1315484e0b304fc
Adam Li <adam2392@gmail.com>
2022-08-29 13:05:43 -0400

    Fix ci
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -38,6 +38,7 @@ sphinx_gallery_conf = {
             "../examples/graphviz_layout",
             "../examples/graphviz_drawing",
             "../examples/graph",
+            "../examples/mixededge",
             "../examples/algorithms",
             "../examples/advanced",
             "../examples/external",
