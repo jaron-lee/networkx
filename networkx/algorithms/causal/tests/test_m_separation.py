08fe51654bfe1555d67db3d2f6510af21e6c28ab
Jaron Lee <jaron2005@gmail.com>
2022-08-30 11:26:59 -0400

    improve m-separation property
    Signed-off-by: Jaron Lee <jaron2005@gmail.com>
    
    revise changes based on suggestions
    
    fix rest of suggested changes
    
    raise error if not acyclic

--- networkx/algorithms/causal/tests/test_m_separation.py
+++ networkx/algorithms/causal/tests/test_m_separation.py
@@ -1,21 +1,18 @@
-import pytest
-
 import networkx as nx
+import pytest
 
 
 def test_m_separation():
     digraph = nx.path_graph(4)
+    digraph.add_edge(2, 4)
     bigraph = nx.Graph([(2, 3)])
     bigraph.add_nodes_from(digraph)
     G = nx.MixedEdgeGraph([digraph, bigraph], ["directed", "bidirected"])
 
     # error should be raised if someone does not use a MixedEdgeGraph
-    with pytest.raises(
-        RuntimeError, match="m-separation should only be run on a MixedEdgeGraph"
-    ):
+    with pytest.raises(RuntimeError, match="m-separation should only be run on a MixedEdgeGraph"):
         nx.m_separated(digraph, {0}, {1}, set())
 
-    print(G.edges())
     # basic d-separation statements based on blocking paths should work
     assert not nx.m_separated(G, {0}, {3}, set())
     assert nx.m_separated(G, {0}, {3}, {1})
@@ -23,3 +20,6 @@ def test_m_separation():
     # conditioning on a collider via bidirected edge opens the collider
     assert not nx.m_separated(G, {0}, {3}, {2})
     assert nx.m_separated(G, {0}, {3}, {1, 2})
+
+    # conditioning on a descendant of a collider via bidirected edge opens the collider
+    assert not nx.m_separated(G, {0}, {3}, {4})
